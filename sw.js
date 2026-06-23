25ad967e91feb44196816cdb1311bd68b1811ae5
tyl114829-afk <tyl114829@gmail.com>
2026-06-23 13:55:06 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="fudan-study-v1";
+const APP=["./","./index.html","./styles.css","./app.js","./culture-data.js","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./背诵系统使用说明.md","./文化要略_30个背诵单元.md","./苹果手机安装说明.md"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match("./index.html"))));
+});
